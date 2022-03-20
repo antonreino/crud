6b9e8735de6d5dc0a6600adbfc1f9e808a651a82
Antón Reino Vela <antonreino@gmail.com>
2022-03-20 15:01:20 +0100

    Update
    Refactorización

--- src/main/java/com/example/demo/configuration/SpringConfiguration.java
+++ src/main/java/com/example/demo/configuration/SpringConfiguration.java
@@ -1,20 +1,33 @@
 package com.example.demo.configuration;
 
-import javax.sql.DataSource;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfiguration {
+
+    @Value("${mysql.driver.url}")
+    String mysqlDriver;
+
+    @Value("${spring.datasource.url}")
+    String mysqlUrl;
+
+    @Value("${spring.datasource.username}")
+    String user;
+
+    @Value("${spring.datasource.password}")
+    String pass;
 	@Bean
     public DataSource datasource() {
         return DataSourceBuilder.create()
-          .driverClassName("com.mysql.cj.jdbc.Driver")
-          .url("jdbc:mysql://localhost:3306/manolito")
-          .username("root")
-          .password("deportivo")
+          .driverClassName(mysqlDriver)
+          .url(mysqlUrl)
+          .username(user)
+          .password(pass)
           .build();	
     }
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,14 +1,13 @@
 package com.example.demo.repository;
 
-import java.io.Serializable;
-
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.User;
+import java.io.Serializable;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Serializable> {
-	
+	User findByUserAndPass(String user,String pass);
 	
 }
